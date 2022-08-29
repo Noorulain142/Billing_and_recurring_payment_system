890898b843c64ec1e82832e28e897ebc00429aa1
Dev <dev@Devs-MacBook-Pro.local>
2022-08-29 19:04:00 +0500

    production changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: "billing-payment-system142.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "billing-system-18575.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
