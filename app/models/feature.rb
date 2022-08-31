76cb4b1d0af53bc618c9a4d54171053402177305
Dev <dev@Devs-MacBook-Pro.local>
2022-08-31 15:20:16 +0500

    validation changes

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,8 +2,10 @@
 
 class Feature < ApplicationRecord
   belongs_to :plan
+
   validates :name, :code, :unit_price, :max_unit_limit, :usage_value, presence: true
   validates :code, :unit_price, :max_unit_limit, :usage_value, numericality: { only_integer: true, greater_than: 0 }
+
   def over_use
     @use = Feature.find(id)
     return @over_use = (usage_value - max_unit_limit) * unit_price if usage_value > max_unit_limit
